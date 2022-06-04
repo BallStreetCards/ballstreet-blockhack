3ce90c750d66c4751af40c203339dbecae3290bb
Alain Nguyen <46081985+Aloinn@users.noreply.github.com>
2022-06-04 12:41:19 -0400

    Adding components for Product Page

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,13 +1,57 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page class="column bg-dark-cream">
+    <product-main />
+    <div class="row q-pa-md-xl q-pa-md q-col-gutter-md">
+      <div class="col-md-7 self-stretch col-12">
+        <product-price-history />
+      </div>
+      <div class="col-md-5 col-12">
+        <product-asset-summary />
+      </div>
+      <!-- VIDEO -->
+      <div class="col-md-7 self-stretch col-12">
+        <product-video />
+      </div>
+      <!-- INFO -->
+      <div class="col-md-5 col-12">
+        <product-cardholder-info />
+      </div>
+
+      <!-- SPORTSDATA -->
+      <div class="col-12">
+        <product-player />
+      </div>
+    </div>
+
+    <product-page-footer />
+  </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useFirestoreCollection } from '@gcto/firebase-hooks';
+import ProductMain from 'src/components/ProductPage/ProductMain.vue';
+import ProductAssetSummary from 'src/components/ProductPage/ProductAssetSummary.vue';
+import ProductPriceHistory from 'src/components/ProductPage/ProductPriceHistory.vue';
+import ProductCardholderInfo from 'src/components/ProductPage/ProductCardholderInfo.vue';
+import ProductPlayer from 'src/components/ProductPage/ProductPlayer.vue';
+import ProductVideo from 'src/components/ProductVideo.vue';
+import ProductPageFooter from '../components/ProductPage/ProductPageFooter.vue';
 
 export default defineComponent({
+  components: {
+    ProductMain,
+    ProductAssetSummary,
+    ProductPriceHistory,
+    ProductCardholderInfo,
+    ProductPlayer,
+    ProductVideo,
+    ProductPageFooter,
+  },
+  name: 'EssentialLink',
   setup() {
-    return {};
+    const { data } = useFirestoreCollection('card-instance', () => ({}));
+    return { data };
   },
 });
 </script>
