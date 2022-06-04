b3f23092387eadf9570c959fcacaec04bc46e12a
Hoang <hoang.viet.nguyen@outlook.com>
2022-06-04 13:45:18 -0400

    added firebase integration for marketplace cards

--- src/pages/MarketplacePage.vue
+++ src/pages/MarketplacePage.vue
@@ -5,8 +5,8 @@
       <marketplace-search />
     </div>
     <div class="container-lg q-mx-auto q-mt-xl gutter-cards q-mb-xl">
-      <template v-if="cards.data">
-        <template v-for="card in cards.data.value" :key="card[0]">
+      <template v-if="cards">
+        <template v-for="card in cards" :key="card[0]">
           <marketplace-card :card="card"></marketplace-card>
         </template>
       </template>
@@ -18,28 +18,21 @@
 import MarketplaceBanner from 'src/components/MarketplacePage/MarketplaceBanner.vue';
 import MarketplaceCard from 'src/components/MarketplacePage/MarketplaceCard/MarketplaceCard.vue';
 import MarketplaceSearch from 'src/components/MarketplacePage/MarketplaceSearch.vue';
+import { db } from 'boot/firebase';
+import { collection, query, onSnapshot } from 'firebase/firestore';
+import Card from 'src/models/Card';
+import { ref } from 'vue';
 
-const cards = {
-  data: {
-    value: [
-      {
-        name: 'Lamelo Ball',
-        imageUrl:
-          'https://firebasestorage.googleapis.com/v0/b/ball-street-20550.appspot.com/o/img%2Flamelo_ball_front.png?alt=media&token=0efb1a50-8c40-4833-b213-67628102811c',
-      },
-      {
-        name: 'Lamelo Ball',
-        imageUrl:
-          'https://firebasestorage.googleapis.com/v0/b/ball-street-20550.appspot.com/o/img%2Flamelo_ball_front.png?alt=media&token=0efb1a50-8c40-4833-b213-67628102811c',
-      },
-      {
-        name: 'Lamelo Ball',
-        imageUrl:
-          'https://firebasestorage.googleapis.com/v0/b/ball-street-20550.appspot.com/o/img%2Flamelo_ball_front.png?alt=media&token=0efb1a50-8c40-4833-b213-67628102811c',
-      },
-    ],
-  },
-};
+const cards = ref<Card[]>([]);
+
+const q = query(collection(db, 'marketplace-cards'));
+onSnapshot(q, (querySnapshot) => {
+  const nCards: Card[] = [];
+  querySnapshot.forEach((doc) => {
+    nCards.push(doc.data() as Card);
+  });
+  cards.value = nCards;
+});
 </script>
 
 <style scoped lang="scss">
