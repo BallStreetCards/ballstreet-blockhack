1b46771ba8573fc5b41bf81184dfeadee0356785
Alain Nguyen <46081985+Aloinn@users.noreply.github.com>
2022-06-09 14:57:54 -0400

    Fixed collection querying

--- quasar/src/pages/MarketplacePage.vue
+++ quasar/src/pages/MarketplacePage.vue
@@ -7,35 +7,28 @@
       </div>
       <div class="container-lg q-mx-auto q-mt-xl gutter-cards q-mb-xl">
         <template v-if="cards">
-          <template v-for="card in cards" :key="card[0]">
+          <div v-for="[cardId, card] in cards" :key="cardId">
             <marketplace-card :card="card"></marketplace-card>
-          </template>
+          </div>
         </template>
+
+        <!-- {{ Object.keys(cards) }} -->
+        <!-- {{ Object.values(cards) }} -->
       </div>
     </div>
   </q-page>
 </template>
 
 <script lang="ts" setup>
-import {
-  collection,
-  query,
-  onSnapshot,
-  getFirestore,
-} from 'firebase/firestore';
+import { useFirestoreCollection } from '@gcto/firebase-hooks/lib';
 import Card from 'src/models/Card';
-import { ref } from 'vue';
+import MarketplaceCard from '../components/MarketplacePage/MarketplaceCard/MarketplaceCard.vue';
+import MarketplaceBanner from 'src/components/MarketplacePage/MarketplaceBanner.vue';
+import MarketplaceSearch from 'src/components/MarketplacePage/MarketplaceSearch.vue';
+import { computed } from 'vue';
 
-const cards = ref<Card[]>([]);
-const db = getFirestore();
-const q = query(collection(db, 'marketplace-cards'));
-onSnapshot(q, (querySnapshot) => {
-  const nCards: Card[] = [];
-  querySnapshot.forEach((doc) => {
-    nCards.push(doc.data() as Card);
-  });
-  cards.value = nCards;
-});
+const cardsCache = useFirestoreCollection<Card>('marketplace-cards');
+const cards = computed(() => [...(cardsCache.data.value?.entries() || [])]);
 </script>
 
 <style scoped lang="scss">
